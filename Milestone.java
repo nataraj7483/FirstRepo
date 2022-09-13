3809fd2f66882b2702bae60fbe903c8437bab687
unknown <natarajs.xworkz@gmail.com>
2022-09-13 11:02:47 +0530

    hello

---
+++ Milestone.java
@@ -0,0 +1,63 @@
+class Milestone{
+
+ static void showsTheResponsibility()
+ {
+  int age=10;
+  char gender='F';
+  if(age==10)
+  {
+	  System.out.println("studying in 6th standard");
+  }
+  else{
+	  System.out.println("completion of 6th standerd");
+  }
+  if(age==18)
+  {
+	  System.out.println("we have rights to vote");
+	  System.out.println("responsibility of driving license");
+	  if(gender=='F')
+	  {
+		  System.out.println("minimum age for girls to get marry");
+	  }
+  }
+  if(age==21)
+  {
+	  System.out.println("minimum age for boys to get marry");
+  }
+  if(age==23||age==24)
+  {
+	  System.out.println("age to get a job");
+  }
+  if(age==28)
+  {
+	  System.out.println("age to plane for kid");
+  }
+  if(age==30)
+  {
+	  System.out.println("plane for second kid");
+  }
+  if(age==45)
+  {
+	  System.out.println("age to think for kids future");
+  }
+  if(age==50)
+  {
+	  System.out.println("age to think for kids education");
+  }
+  if(age==58)
+  {
+	  System.out.println("age for retired");
+  }
+  if(age==60)
+  {
+	  System.out.println("age for playing grand childrens");
+  }
+  if(age==70)
+  {
+	  System.out.println("age for RIP");
+  }
+  
+ }
+
+
+}
