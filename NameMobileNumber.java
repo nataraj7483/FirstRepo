51e476ffff0b3aaef446f32f81a64293e714184e
nataraj7483 <113172232+nataraj7483@users.noreply.github.com>
2022-09-14 21:57:02 +0530

    Add files via upload

---
+++ NameMobileNumber.java
@@ -0,0 +1,66 @@
+class NameMobileNumber{
+	
+	static String getNameByMobile(long number){
+	System.out.println("entered getNameByMobile : ");
+	
+	if(number==7483735838L)
+	{
+		System.out.println("Nataraj no");
+		return "Nataraj";
+	}
+	if(number==7353865749L)
+    {
+		System.out.println("Chandru no");
+		return "Chandru";
+	}
+    if(number==7483733858L)
+    {
+		System.out.println("Maruthi no");
+		return "Maruthi";
+	}
+	if(number==7353865794L)
+	{
+		System.out.println("Abhishek no");
+		return "Abhishek";
+	}
+	if(number==7483735898L)
+	{
+		System.out.println("Bharath no");
+		return "Bharath";
+	}
+	if(number==7353865787L)
+	{
+		System.out.println("Anil no");
+		return "Anil";
+	}
+	if(number==8747872249L)
+	{
+		System.out.println("Shivraj no");
+		return "Shivraj";
+	}
+	if(number==8747020465L)
+	{
+		System.out.println("Manjunath no");
+		return "Manjunath";
+	}
+	if(number==8197732645L)
+	{
+		System.out.println("Shreyas no");
+		return "Shreyas";
+	}
+	if(number==8197732674L)
+	{
+		System.out.println("Raviteej no");
+		return "Raviteej";
+	}
+	return null;
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
---
+++ NameMobileNumberRunner.java
@@ -0,0 +1,14 @@
+class NameMobileNumberRunner{
+	
+	public static void main(String[] numbers)
+	{
+	 long mobile=7483735868L;
+     String ref=NameMobileNumber.getNameByMobile(mobile);	 
+	System.out.println(ref);	
+		
+	}
+	
+	
+	
+	
+}
