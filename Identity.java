d9f058edad576a33969ff692ac7ed9103bfb1c17
nataraj7483 <113172232+nataraj7483@users.noreply.github.com>
2022-09-14 21:59:41 +0530

    Add files via upload

---
+++ Identity.java
@@ -0,0 +1,26 @@
+class Identity{
+	static void printName(String name)
+	{
+		System.out.println("entered printName");
+		System.out.println("Name entered:"+name);
+	}
+	static void printNameAndEmail(String name,String email)
+	{
+		System.out.println("entered printNameAndEmail");
+		System.out.println("Name"+name);
+		System.out.println("Email"+email);
+	}
+	static void printNameAndAge(String name,int age)
+	{
+		System.out.println("entered printNameAndAge");
+		System.out.println("Name"+name);
+		System.out.println("Age"+age);
+	}
+	static void printNameAndAddress(String name,String address)
+	{
+		System.out.println("entered printNameAndAddress");
+		System.out.println("Name"+name);
+		System.out.println("Address"+address);
+	}
+}
+	
---
+++ IdentityRunner.java
@@ -0,0 +1,64 @@
+class IdentityRunner{
+	public static void main(String[] names)
+	{
+		System.out.println("entered main...");
+		String value=("jaideep");
+		Identity.printName(value);
+		Identity.printName("Nataraj");
+		Identity.printName("sanjay");
+		Identity.printName("sharath");
+		Identity.printName("shashank");
+		Identity.printName("Arun");
+		Identity.printName("Shashi");
+		Identity.printName("Prashanth");
+		Identity.printName("Raviteej");
+		Identity.printName("Saikumar");
+		Identity.printName("Naveen");
+		Identity.printName("Harish");
+		Identity.printName("Ravi");
+		Identity.printName("Maruthi");
+		
+		Identity.printNameAndEmail("Nataraj","Nataraj.xworkz@gmail.com");
+		Identity.printNameAndEmail("Sanjay","Sanjay.xworkz@gmail.com");
+		Identity.printNameAndEmail("sharath","Sharath.xworkz@gmail.com");
+		Identity.printNameAndEmail("shashank","Shashank.xworkz@gmail.com");
+		Identity.printNameAndEmail("Arun","Arun.xworkz@gmail.com");
+		Identity.printNameAndEmail("shashi","Shashi.xworkz@gmail.com");
+		Identity.printNameAndEmail("Prashanth","Prashanth.xworkz@gmail.com");
+		Identity.printNameAndEmail("Raviteej","Raviteej.xworkz@gmail.com");
+		Identity.printNameAndEmail("Saikumar","Saikumar.xworkz@gmail.com");
+		Identity.printNameAndEmail("Naveen","Naveen.xworkz@gmail.com");
+		Identity.printNameAndEmail("Harish","Harish.xworkz@gmail.com");
+		Identity.printNameAndEmail("Ravi","Ravi.xworkz@gmail.com");
+		
+		Identity.printNameAndAge("Nataraj",32);
+		Identity.printNameAndAge("Sanjay",22);
+		Identity.printNameAndAge("Sharath",18);
+		Identity.printNameAndAge("Shashank",17);
+		Identity.printNameAndAge("Arun",21);
+		Identity.printNameAndAge("Shashi",52);
+		Identity.printNameAndAge("Prashanth",16);
+		Identity.printNameAndAge("Raviteej",42);
+		Identity.printNameAndAge("Saikumar",12);
+		Identity.printNameAndAge("Naveen",20);
+		Identity.printNameAndAge("Harish",48);
+		Identity.printNameAndAge("Ravi",32);
+		Identity.printNameAndAge("Maruthi",66);
+		
+		Identity.printNameAndAddress("Nataraj","Hosadurga chitradurga");
+		Identity.printNameAndAddress("Sanjay","Davangere Davangere");
+		Identity.printNameAndAddress("Sharath","kadure ");
+		Identity.printNameAndAddress("Shashank","Hosadurga Chitradurga");
+		Identity.printNameAndAddress("Arun","Davangere");
+		Identity.printNameAndAddress("Shashi","Hosadurga");
+		Identity.printNameAndAddress("prashanth","Chikmangaloore");
+		Identity.printNameAndAddress("Raviteej","Shivmoga");
+		Identity.printNameAndAddress("Saikumar","Tipture");
+		Identity.printNameAndAddress("Naveen","Koramangala Bangaloore");
+		Identity.printNameAndAddress("Harish","Koramangala Banglore");
+		Identity.printNameAndAddress("Ravi","Badravathi Shivamoga");
+		
+		
+		
+	}
+}
