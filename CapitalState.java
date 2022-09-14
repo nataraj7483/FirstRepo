fade3433de4b776d0fd65994aa6f3025dd006839
nataraj7483 <113172232+nataraj7483@users.noreply.github.com>
2022-09-14 21:55:54 +0530

    Add files via upload

---
+++ CapitalState.java
@@ -0,0 +1,150 @@
+class CapitalState{
+	
+	static String getcapitalByState(String state)
+	{
+		System.out.println("Entered getcapitalByState");
+	if(state=="Karnataka")
+	{
+		System.out.println("Karnataka state capital is");
+		return "Bangaloore";
+	}
+	if(state=="Tamilnadu")
+	{
+		System.out.println("Tamilnadu state capital is");
+		return "Chennai";
+	}
+	if(state=="Andra pradesh")
+    {
+		System.out.println("Andra pradesh state capital is");
+		return "Amaravathi";
+	}		
+	if(state=="Arunachal pradesh")
+	{
+		System.out.println("Arunachal pradesh state capital is");
+		return "itengar";
+	}
+	if(state=="Assam")
+	{
+		System.out.println("Assam state capital is");
+		return "Dispur";
+	}
+	if(state=="Bihar")
+	{
+		System.out.println("Bihar state capital is");
+		return "Patna";
+	}
+	if(state=="Chattisgarth")
+	{
+		System.out.println("Chattisgarth state capital is");
+		return "Raipur";
+	}
+	if(state=="Goa")
+	{
+		System.out.println("Goa state capital is");
+		return "panaji";
+	}
+	if(state=="Gujarat")
+	{
+		System.out.println("Gujarath state capital is");
+		return "Gandhinaga";
+	}
+	if(state=="Haryana")
+	{
+		System.out.println("Haryana state capital is");
+		return "Chandigarh";
+	}
+	if(state=="Himachal pradesh")
+	{
+		System.out.println("Himachal pradesh state capital is");
+		return "Shimla";
+	}
+	if(state=="Jharkhand");
+	{
+		System.out.println("Jharkhand state capital is");
+		return "Ranchi";
+	}
+	if(state=="Kerala")
+	{
+		System.out.println("Kerala state capital is");
+		return "thiruvananthapuram";
+	}
+	if(state=="Madhya pradesh")
+	{
+		System.out.println("Madhya pradesh state capital is");
+		return "bhopal";
+	}
+	if(state=="Maharastra")
+	{
+		System.out.println("Maharastra state capital is");
+		return "mumbai";
+	}
+	if(state=="Manipur")
+	{
+		System.out.println("Manipur state capital is");
+		return "imphal";
+	}
+	if(state=="Meghalaya")
+    {
+		System.out.println("Meghalaya state capital is");
+		return "Shillog";
+	}
+	if(state=="Mizoram")
+	{
+		System.out.println("Mizoram state capital is");
+		return "Aizawal";
+	}
+	if(state=="Nagaland")
+	{
+		System.out.println("Nagaland state capital is");
+		return "Kohima";
+	}
+	if(state=="Odisha")
+	{
+		System.out.println("Odisha state capital is");
+		return "Bhubaneshwar";
+	}
+	if(state=="Punjab")
+	{
+		System.out.println("Punjab state capital is");
+		return "Chandigarh";
+	}
+	if(state=="Rajastan")
+	{
+		System.out.println("Rajastan state capital is");
+		return "Jaipur";
+	}
+	if(state=="Sikkim")
+	{
+		System.out.println("Sikkim state capital is");
+		return "Gangtok";
+	}
+	if(state=="Telangana")
+	{
+		System.out.println("Telangana state capital is");
+		return "Hyderbad";
+	}
+	if(state=="Tripura")
+	{
+		System.out.println("Tripura state capital is");
+		return "Agartala";
+	}
+	if(state=="Uttar pradesh")
+	{
+		System.out.println("Uttar pradesh state capital is");
+		return "Lucknow";
+	}
+	if(state=="Uttarkhand")
+	{
+		System.out.println("Uttarkhand state capital state is");
+		return "Dehradun";
+	}
+	if(state=="West Bengal")
+	{
+		System.out.println("West Bengal state capital is");
+		return "Kolkata";
+	}
+	return null;	
+	}
+	
+	
+}
---
+++ CapitalStateRunner.java
@@ -0,0 +1,12 @@
+class CapitalStateRunner{
+	
+	public static void main(String[] capitals)
+    {
+	    String ref=CapitalState.getcapitalByState("Kerala");
+		System.out.println(ref);
+	}	
+	
+	
+	
+	
+}
