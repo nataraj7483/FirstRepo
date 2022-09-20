3d69b2024f6dda5409b916e82cb7d3cbc6636d3f
nataraj7483 <113172232+nataraj7483@users.noreply.github.com>
2022-09-20 22:22:52 +0530

    Add files via upload

---
+++ CardRunner.java
@@ -0,0 +1,15 @@
+class CardRunner{
+public static void main(String[] args){ 
+	
+	Card.displayDetails();
+	Card.type="Credit Card";
+	Card.height="13cm";
+	Card.width=28;
+	Card.price=36.880d;
+	String[] colour={"Mirage Silver","Pine Green","Lake Blue","Grey"};
+	Card.color=colour;
+	Card.displayDetails();
+}
+
+
+}
