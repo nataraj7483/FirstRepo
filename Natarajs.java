cfcbad6e3e550f4d7314ff02e26aec728bb72b3d
nataraj7483 <113172232+nataraj7483@users.noreply.github.com>
2022-09-30 09:50:56 +0530

    Add files via upload

---
+++ Natarajs.java
@@ -0,0 +1,76 @@
+1.Platform independence 
+  java is a platform independence,because once we write the program execute in any platform.java code will be compile in to a bytecode 
+  this byte code is Platform independence.
+  
+  
+  
+2.Difference b/w 
+ primitive                              Non primitive
+ *It is pre- defined                *It is not pre-defined
+ *It containce datatype             * N number of datatype
+ *It have states and behaviours     *It does not have states and behaviours
+ 
+ 
+3.class: is a blueprint of design. can't be touched and expirienced.
+  It containce states and behaviours.
+  again variables can be declared as both static and non static.
+ 
+
+		   
+4. class percentage{
+	
+	satic double totalMarks(int totalMarks, int received )
+	{
+	  	double percentage=(percentage)*100;
+		return percentage;
+		
+	}
+	
+}
+
+   class percentageRunner{
+	   public static void main(String[] args)
+	   {
+		   system.out.println("Percentage obtained:"+ percentage.marks );
+	   }
+	   
+	   
+	}
+   
+5. class DuplicateNumber{
+	
+	public static void main(String[] args)
+
+	{
+		static int[] values{34,78,90,53,34,11,53,15,78,91,8,4,67,11};
+		for(int index=0; index=values.length)
+		{
+			System.out.println("checking duplicate number");
+			if(values[index]=values[index+1])
+		    {
+				System.out.println("duplice found");
+			}
+			else{
+				System.out.println("duplicata not found");
+			}
+			index++;
+		}
+		
+		
+	}
+	
+	
+}
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
